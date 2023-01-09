20532473964aabc805eb8ae580b9ea71113805b8
Vicente Lermanda <vlermanda@MacBook-Air.local>
2023-01-09 01:31:36 -0300

    add box button

--- scripts.js
+++ scripts.js
@@ -1,9 +1,55 @@
-let d = new Date();
-document.body.innerHTML = "<h1>Today's date is " + d + "</h1>";
+// Create Title
+function createTitle(text) {
+  var title = document.createElement("h1");
+  title.classList.add("text-2xl");
+  title.innerText = text;
+  title.addEventListener('dblclick', editText);
+  return title;
+}
 
-document.getElementById('add-box').addEventListener('click', function(){
-  document.getElementById('boxes').classList.add('element');
+// Title Change
+function editText() {
+  const currentText = this.innerText;
+  
+  // Create an input element
+  const input = document.createElement('input');
+  input.value = currentText;
+  
+  this.parentNode.insertBefore(input, this);
+  this.style.display = 'none';
+  
+  // Save changes when the input element loses focus
+  input.addEventListener('blur', saveChanges);
+}
+
+function saveChanges() {
+  const newText = this.value;
+  parentt = this.parentNode; 
+  parentt.removeChild(this);
+  parentt.appendChild(createTitle(newText));
+}
+
+// Button Box
+const button = document.getElementById("add-box-button");
+var num = 1;
+
+button.addEventListener('click', () => {
+  // create and define new box/container
+  var newBox = document.createElement("div");
+  newBox.classList.add("element");
+  newBox.id = num;
+
+  // adding title
+  var title = createTitle("New title");
+  newBox.appendChild(title);
+
+  // button to add li
+
+  // insert new box
+  const boxes = document.getElementById("boxes");
+  boxes.insertBefore(newBox, button);
+
+  num++;
 });
-
 
 
