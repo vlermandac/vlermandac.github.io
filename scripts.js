11823dc58e6c1f00e392173f559d4cba482664b3
Vicente Lermanda <vlermanda@MacBook-Air.local>
2023-01-09 02:58:11 -0300

    stuff

--- scripts.js
+++ scripts.js
@@ -3,12 +3,12 @@ function createTitle(text) {
   var title = document.createElement("h1");
   title.classList.add("text-2xl");
   title.innerText = text;
-  title.addEventListener('dblclick', editText);
+  title.addEventListener('dblclick', editTitleText);
   return title;
 }
 
 // Title Change
-function editText() {
+function editTitleText() {
   const currentText = this.innerText;
   
   // Create an input element
@@ -19,15 +19,47 @@ function editText() {
   this.style.display = 'none';
   
   // Save changes when the input element loses focus
-  input.addEventListener('blur', saveChanges);
+  input.addEventListener('blur', saveTitleChanges);
 }
 
-function saveChanges() {
+function saveTitleChanges() {
   const newText = this.value;
   parentt = this.parentNode; 
   parentt.removeChild(this);
-  parentt.appendChild(createTitle(newText));
+  parentt.insertBefore(createTitle(newText), parentt.firstChild);
 }
+
+// Create li
+function createLi(text) {
+  var title = document.createElement("li");
+  title.innerText = text;
+  title.addEventListener('dblclick', editLiText);
+  return title;
+}
+
+// Li Change
+function editLiText() {
+  const currentText = this.innerText;
+  
+  // Create an input element
+  const input = document.createElement('input');
+  input.value = currentText;
+  
+  this.parentNode.insertBefore(input, this);
+  this.style.display = 'none';
+  
+  // Save changes when the input element loses focus
+  input.addEventListener('blur', saveLiChanges);
+}
+
+function saveLiChanges() {
+  const newText = this.value;
+  nextLi = this.nextSibling;
+  parentt = this.parentNode; 
+  parentt.removeChild(this);
+  parentt.insertBefore(createLi(newText), nextLi);
+}
+
 
 // Button Box
 const button = document.getElementById("add-box-button");
@@ -43,7 +75,21 @@ button.addEventListener('click', () => {
   var title = createTitle("New title");
   newBox.appendChild(title);
 
+  // adding unordered list
+  var ul = document.createElement("ul");
+  var liFirst = document.createElement("li");
+  var liButton = document.createElement('button'); //ADD TEXT
+  liButton.classList.add('text-2xl');
+  liButton.textContent = "+";
+  liButton.addEventListener('click', () => {
+    var newLi = createLi("New item");
+    ul.insertBefore(newLi, liFirst);
+  });
+  liFirst.appendChild(liButton);
+
   // button to add li
+  ul.appendChild(liFirst);
+  newBox.appendChild(ul);
 
   // insert new box
   const boxes = document.getElementById("boxes");
