41e42039db881fa6ed33468f8228a7eab0fee90b
Vicente Lermanda <vlermanda@MacBook-Air.local>
2023-01-14 20:47:00 -0300

    adding stuff

--- scripts.js
+++ scripts.js
@@ -16,7 +16,9 @@ class Title{
     item.style.display = 'none';
     input.addEventListener('blur', () => {
       var p = input.parentNode;
-      p.insertBefore(new Title(input.value, this.parentBoxID), p.firstChild);
+      var tmp = new Title(input.value, this.parentBoxID);
+      this.title.parentNode.title = tmp;
+      p.insertBefore(tmp, p.firstChild);
       p.removeChild(input);
       localStorage.setItem(this.parentBoxID, this.title.parentNode.outerHTML);
     });
@@ -24,24 +26,32 @@ class Title{
 }
 
 class Box {
-  constructor(id){
+  constructor(id, title, elements){
     this.box = document.createElement("div");
     this.box.id = id;
     this.box.classList.add("element");
-    this.box.appendChild(new Title("New title", id));
-    this.ul = new ItemList(id);
+    if(title == undefined) var title = "New title";
+    this.title = new Title(title, id);
+    this.box.appendChild(this.title);
+    if(elements == undefined)
+      this.ul = new ItemList(id);
+    else
+      this.ul = new ItemList(id, elements);
     this.box.appendChild(this.ul.ul);
   }
 }
 
 class ItemList {
-  constructor(parentBoxID){
+  constructor(parentBoxID, elements){
     this.id = parentBoxID;
     this.ul = document.createElement("ul");
     this.liFirst = document.createElement("li");
     this.addItemButton = this.addButton(); 
     this.liFirst.appendChild(this.addItemButton);
     this.ul.appendChild(this.liFirst);
+    if(elements != undefined)
+      elements.forEach((element) => 
+        this.ul.insertBefore(this.addItem(element), this.liFirst));
   }
 
   addButton(){
@@ -71,6 +81,7 @@ class ItemList {
       var p = input.parentNode;
       p.insertBefore(this.addItem(input.value), input.nextSibling);
       p.removeChild(input);
+      item.parentNode.removeChild(item);
       localStorage.setItem(this.id, this.ul.parentNode.outerHTML);
     });
   }
@@ -95,7 +106,15 @@ for (let i = 1; i < id; i++) {
   var storedBox = localStorage.getItem(i)
   var newBox = new Box(i);
   newBox.box.innerHTML = storedBox;
-  boxes.insertBefore(newBox.box, button);
+  let h1 = newBox.box.querySelector('h1').innerText;
+  let ul = newBox.box.querySelector('ul');
+  ul.removeChild(ul.lastChild);
+  let list = Array.from(ul.children, li => li.innerText);
+  console.log(ul);
+  console.log(list);
+  console.log("***\n");
+  var retrievedBox = new Box(i, h1, list);
+  boxes.insertBefore(retrievedBox.box, button);
 }
 
 // add listener to add-box button (defined in HTML)
