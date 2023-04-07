2945b9551aba183a0e20c6d7bbd3d45b7719a310
Vicente Lermanda <68314874+vlermandac@users.noreply.github.com>
2023-04-07 14:54:33 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,22 +1,31 @@
-$(document).ready(function () {
-  const storageKey = "boxes";
-  const $boxes = $("#boxes");
-  const $addBoxButton = $("#add-box-button");
+class BoxManager {
+  constructor() {
+    this.storageKey = "boxes";
+    this.boxesContainer = $("#boxes");
+    this.addBoxButton = $("#add-box-button");
 
-  function loadBoxes() {
-    const savedBoxes = JSON.parse(localStorage.getItem(storageKey)) || [];
+    this.addBoxButton.on("click", () => {
+      this.createBox("New box");
+      this.saveBoxes();
+    });
+
+    this.loadBoxes();
+  }
+
+  loadBoxes() {
+    const savedBoxes = JSON.parse(localStorage.getItem(this.storageKey)) || [];
     savedBoxes.forEach((box) => {
-      createBox(box.title, box.items);
+      this.createBox(box.title, box.items);
     });
   }
 
-  function saveBoxes() {
-    const boxesData = $boxes
+  saveBoxes() {
+    const boxesData = this.boxesContainer
       .children()
       .map(function () {
-        const $box = $(this);
-        const title = $box.find(".box-title").text();
-        const items = $box
+        const box = $(this);
+        const title = box.find(".box-title").text();
+        const items = box
           .find("ul")
           .children()
           .map(function () {
@@ -26,49 +35,59 @@ $(document).ready(function () {
         return { title, items };
       })
       .get();
-    localStorage.setItem(storageKey, JSON.stringify(boxesData));
+    localStorage.setItem(this.storageKey, JSON.stringify(boxesData));
   }
 
-  function createBox(title, items = []) {
-    const $box = $(`
-      <div class="box p-4 border border-gray-300 rounded">
+  createBox(title, items = []) {
+    const box = $(`
+      <div class="box p-4 border border-gray-300 rounded bg-magenta-500">
         <h2 class="box-title text-xl mb-4" contenteditable="true">${title}</h2>
         <ul class="list-disc list-inside mb-4"></ul>
-        <button class="add-item-button text-lg px-2 py-1 bg-blue-500 text-white rounded">Add Item</button>
+        <input class="new-item-input hidden" type="text" placeholder="Enter a new item...">
+        <button class="add-item-button text-lg px-2 py-1 bg-blue-500 text-white rounded">
+          <i class="fas fa-plus"></i>
+        </button>
       </div>
     `);
 
-    const $list = $box.find("ul");
+    const list = box.find("ul");
     items.forEach((item) => {
-      $list.append(`<li>${item}</li>`);
+      list.append(`<li>${item}</li>`);
     });
 
-    $box.on("dblclick", ".box-title", function () {
+    box.on("dblclick", ".box-title", function () {
       this.contentEditable = "true";
     });
 
-    $box.on("blur", ".box-title", function () {
+    box.on("blur", ".box-title", () => {
       this.contentEditable = "false";
-      saveBoxes();
+      this.saveBoxes();
     });
 
-    $box.on("click", ".add-item-button", function () {
-      const item = prompt("Enter a new item:");
-      if (item) {
-        $list.append(`<li>${item}</li>`);
-        saveBoxes();
+    box.on("click", ".add-item-button", () => {
+      const input = box.find(".new-item-input");
+      input.toggleClass("hidden");
+      input.focus();
+    });
+
+    box.on("keypress", ".new-item-input", (e) => {
+      if (e.key === "Enter") {
+        const item = $(e.target).val().trim();
+        if (item) {
+          list.append(`<li>${item}</li>`);
+          this.saveBoxes();
+        }
+        $(e.target).val("");
+        $(e.target).addClass("hidden");
       }
     });
 
-    $boxes.append($box);
+    this.boxesContainer.append(box);
   }
+}
 
-  $addBoxButton.on("click", function () {
-    createBox("New box");
-    saveBoxes();
-  });
-
-  loadBoxes();
+$(document).ready(function () {
+  new BoxManager();
 });
 
 
